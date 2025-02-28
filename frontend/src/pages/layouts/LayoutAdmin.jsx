df9542b83473d8ff9b1f21e316127fd968553db7
Srnishappy <suranatsuwannarat@gmail.com>
2025-02-28 14:31:42 +0700

    update

--- frontend/src/pages/layouts/LayoutAdmin.jsx
+++ frontend/src/pages/layouts/LayoutAdmin.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../../components/admin/Sidebar';
+import Header from '../../components/admin/Header';
 
 const LayoutAdmin = () => {
   return (
-    <div>
-      <h1>Sidebar</h1>
-      <h1>Header</h1>
-      <Outlet />
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
+        <Header />
+        <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
--- frontend/src/routes/ProtectRouteAdmin.jsx
+++ frontend/src/routes/ProtectRouteAdmin.jsx
@@ -1,19 +1,35 @@
 import { useEffect, useState } from 'react';
 import useEcomStore from '../store/ecom-store';
 import { currentAdmin } from '../api/Auth';
-import LoadingToRedirect from './LoadingToRedirect';
-const ProtectRouteAdmin = ({ element }) => {
+import LoadingToRedirect from '../routes/LoadingToRedirect';
+
+const ProtectRouteUser = ({ element }) => {
   const [ok, setOk] = useState(false);
+  const [loading, setLoading] = useState(true); // เพิ่มตัวแปร loading
   const user = useEcomStore((state) => state.user);
   const token = useEcomStore((state) => state.token);
+
   useEffect(() => {
     if (user && token) {
       currentAdmin(token)
-        .then((res) => setOk(true))
-        .catch((err) => setOk(false));
+        .then((res) => {
+          setOk(true);
+          setLoading(false); // อัพเดตสถานะเป็น false เมื่อโหลดเสร็จ
+        })
+        .catch((err) => {
+          setOk(false);
+          setLoading(false); // อัพเดตสถานะเป็น false เมื่อโหลดเสร็จ
+        });
+    } else {
+      setLoading(false); // หากไม่มี user หรือ token ให้ตั้งสถานะ loading เป็น false
     }
-  }, []);
+  }, [user, token]);
+
+  if (loading) {
+    return <div>Loading...</div>; // แสดงข้อความหรือ spinner ในระหว่างที่กำลังโหลด
+  }
 
   return ok ? element : <LoadingToRedirect />;
 };
-export default ProtectRouteAdmin;
+
+export default ProtectRouteUser;
